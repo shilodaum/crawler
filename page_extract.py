bf21b3ae37274b86a444b642d19e97a9d0de75e2
shilodaum <32439775+shilodaum@users.noreply.github.com>
2021-11-18 21:32:36 +0200

    changed to kickstarter

--- page_extract.py
+++ page_extract.py
@@ -1,10 +1,8 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup as bs
 import scrapy
 import time
-
-def extract_fields_from_html(html):
-    fields=['owner_name',]
+import selenium
 
 # Fields:
 # Creators: owner_name
@@ -18,14 +16,39 @@ def extract_fields_from_html(html):
 # InDemand <meta name="sailthru.funding_period" content="indemand" />
 # FlexibleGoal:"is_fixed_funding":true
 
-main_url='https://www.indiegogo.com/explore/home?project=all&project=all&sort=trending'
+main_url = 'https://www.kickstarter.com/discover/categories/technology?page='
 
-def test_one_web():
+
+def crawl():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/70.0.3538.77 Safari/537.36"}
-    adress=main_url
-    r = requests.get(adress, headers=headers)
-    with open("itemlist.html",'w') as f:
-        f.write(r.text)
-test_one_web()
+    address = main_url+"0"
+
+    r = requests.get(address, headers=headers)
+    if r.status_code == 200:
+        with
+        return r.text
+
+
+def get_fields(html: str):
+    driver = webdriver.Firefox()
+    driver.get("https://www.indiegogo.com/explore/home?project_type=campaign&project_timing=all&sort=trending")
+    print(driver.title)
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    #e#lem.send_keys("pycon")
+    #e#lem.send_keys(Keys.RETURN)
+    #assert "No results found." not in driver.page_source
+    driver.close()
+
+
+def main():
+    with open("page_exaple.html", 'r') as f:
+        h = f.read()
+    # h = test_one_web()
+    get_fields(h)
+
+
+if __name__ == '__main__':
+    main()

3b086da9e4646e02ea6bae10d66184cd2e51c9a3
inbalmishal <inbal.mishal3@gmail.com>
2021-11-19 01:12:15 +0200

    fix with_scrapy.py

--- with_scrapy.py
+++ with_scrapy.py
@@ -14,11 +14,14 @@ def download_file():
 
 
 def find_attribute_by_regex(prefix, variable_reg, suffix, html_txt):
-    regex = prefix + variable_reg + suffix
+    regex = prefix + variable_reg
     full_row = re.findall(regex, html_txt)
     without_prefix = re.sub(prefix, "", full_row[0])
     idx = without_prefix.find(suffix)
-    result = without_prefix[:idx]
+    if idx != -1:
+        result = without_prefix[:idx]
+    else:
+        result = without_prefix[:]
     # result = re.sub(suffix , "", without_prefix)
     # # print(result)
     return result
@@ -30,69 +33,37 @@ def get_attributes():
 
     # regex for the title:
     # <meta property="og:title" content="Revopoint POP 2: Precise 3D Scanner with 0.1mm Accuracy"/>
-    title = find_attribute_by_regex(r"<meta property=\"og:title\" content=\"", r".+", r"\"/>", html_txt)
+    title = find_attribute_by_regex(r"<meta property=\"og:title\" content=\"", r".+", "\"/>", html_txt)
     print('title =', title)
 
     # regex for the creator
-    #
+    # <div class="type-14 bold clip ellipsis"> Revopoint 3D </div><div class="mr2">
+    creator = find_attribute_by_regex(r"<div class=\"type-14 bold clip ellipsis\">", r".+", "</div><div class=\"mr2\">", html_txt)
+    print('creator = ', creator)
 
     # Text (entire html)
     text = html_txt
 
     # DollarsPledged
-    # converted_pledged_amount&quot;:617454,
-    DollarsPledged = find_attribute_by_regex(r"converted_pledged_amount&quot;:", r"[0-9]+", r",", html_txt)
+    DollarsPledged = find_attribute_by_regex(r"converted_pledged_amount&quot;:", r"[0-9]+", ",", html_txt)
     print('DollarsPledged =', DollarsPledged)
 
     # DollarGoal
-    # "project_goal_usd":9975.29,
-    DollarGoal = find_attribute_by_regex(r"\"project_goal_usd\":", r'[0-9]+\.?[0-9]+', r",", html_txt)
+    DollarGoal = find_attribute_by_regex(r"\"project_goal_usd\":", r'[0-9]+\.?[0-9]+', ",", html_txt)
     print('DollarGoal =', DollarGoal)
 
     # NumBackers
-    # backers_count&quot;:1341,&quot;
-    NumBackers = find_attribute_by_regex(r"backers_count&quot;:", r"[0-9]+", r",", html_txt)
+    NumBackers = find_attribute_by_regex(r"backers_count&quot;:", r"[0-9]+", ",", html_txt)
     print('NumBackers =', NumBackers)
 
     # DaysToGo
-    # <span class="block type-16 type-28-md bold dark-grey-500"> #### </span>
     DaysToGo = find_attribute_by_regex(r"<span class=\"block type-16 type-28-md bold dark-grey-500\">", r"[0-9]+",
-                                       r"</span>", html_txt)
+                                       "</span>", html_txt)
     print('DaysToGo =', DaysToGo)
 
     # AllOrNothing
-    # <span data-test-id="deadline-exists"> ### This project will only be funded if it reaches its goal by Sat, January 1 2022 3:00 PM UTC +02:00. ### </span>
-    AllOrNothing = find_attribute_by_regex(r"<span data-test-id=\"deadline-exists\">", r".*", r"</span>", html_txt)
+    AllOrNothing = find_attribute_by_regex(r"<span data-test-id=\"deadline-exists\">", r".*", "</span>", html_txt)
     print('AllOrNothing = ', AllOrNothing)
-
-
-def new():
-    url = "https://www.rottentomatoes.com/top/bestofrt/"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
-    }
-    f = requests.get(url, headers=headers)
-
-    movies_lst = []
-
-    soup = BeautifulSoup(f.content, 'lxml')
-    movies = soup.find('table', {
-        'class': 'table'}).find_all('a')
-
-    num = 0
-    for anchor in movies:
-        urls = 'https://www.rottentomatoes.com' + anchor['href']
-
-    movies_lst.append(urls)
-    num += 1
-    movie_url = urls
-    movie_f = requests.get(movie_url, headers=headers)
-    movie_soup = BeautifulSoup(movie_f.content, 'lxml')
-    movie_content = movie_soup.find('div', {
-        'class': 'movie_synopsis clamp clamp-6 js-clamp'
-    })
-    print(num, urls, '\n', 'Movie:' + anchor.string.strip())
-    print('Movie info:' + movie_content.string.strip())
 
 
 if __name__ == '__main__':
